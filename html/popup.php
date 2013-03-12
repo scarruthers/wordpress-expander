20e63be59d73985aa450fb62f1d2d0fa464758da
Sean Carruthers <sean_carruthers@hotmail.com>
2013-03-12 02:06:49 -0500

    .

--- html/popup.php
+++ html/popup.php
@@ -14,25 +14,25 @@
 								<input type='text' name='title' id='expander_title' />
 							</label>
 							<br /><br /><br />
-							
+
 							<?php
 							$args = array(
 								'media_buttons' => false,
 								'textarea_rows' => 5,
 								'tabindex' => 2,
-								'teeny' => true,
+								'teeny' => false,
 								'quicktags' => false
 							);
-							wp_editor('', 'expandercontent', $args); 
-							
+							wp_editor('', 'expandercontent', $args);
+
 							?>
 						</div>
 						<br />
-						
+
 						<label class='clickable'>Show content on page load?
 							<input type='checkbox' name='auto_load' id='expander_auto_load' value='yes' />
 						</label>
-						
+
 						<div class='nonclickable'>
 							<a href='#' class='button add_expander'>Insert Into Page</a>
 							<a href='#' class='close'>Cancel</a>
